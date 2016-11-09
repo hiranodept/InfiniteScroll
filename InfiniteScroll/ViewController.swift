44a41cbd1844409aaa2c6229a895eef22248e98e
Tetsuya Hirano <hirano@hirano-dept.com>
2016-11-09 15:08:58 +0900

    Initial Commit

---
+++ InfiniteScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InfiniteScroll
+//
+//  Created by Tetsuya Hirano on 2016/11/09.
+//  Copyright © 2016年 Tetsuya Hirano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
